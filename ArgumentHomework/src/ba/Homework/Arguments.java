a058c40d9ea8b2933beabbdc27ff8ba0a813d8f9
mladen91 <mladen.teofilovic@bitcamp.ba>
2015-06-08 22:17:18 +0200

    Homework Arguments 08.06.2015.

---
+++ ArgumentHomework/src/ba/Homework/Arguments.java
@@ -0,0 +1,31 @@
+package ba.Homework;
+
+import java.util.InputMismatchException;
+
+public class Arguments {
+
+	public static void main(String[] args) {
+		float product = 1;
+
+		try {
+
+			for (String num : args) {
+
+				float number = Float.parseFloat(num);
+				if (number == 0 || number==' ') {
+					System.exit(0);
+				} else
+
+					System.out.printf("%.2f \n", number);
+
+				product *= number;
+
+			}
+			System.out.printf("Product of our number is equal to: %.2f",
+					product);
+		} catch (InputMismatchException ex) {
+			System.out.println("Wrong input!");
+		}
+	}
+
+}
