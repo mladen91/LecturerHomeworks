3e63bbc59d3b5b17f26ff0cb1afda3802c352d40
mladen91 <mladen.teofilovic@bitcamp.ba>
2015-06-09 18:34:05 +0200

    Argument Homework correction

--- ArgumentHomework/src/ba/Homework/Arguments.java
+++ ArgumentHomework/src/ba/Homework/Arguments.java
@@ -12,19 +12,21 @@ public class Arguments {
 			for (String num : args) {
 
 				float number = Float.parseFloat(num);
-				if (number == 0 || number==' ') {
+				if (number == 0 || num.equals(" ")) {
 					System.exit(0);
-				} else
+				} else {
 
 					System.out.printf("%.2f \n", number);
 
-				product *= number;
-
+					product *= number;
+				}
 			}
 			System.out.printf("Product of our number is equal to: %.2f",
 					product);
 		} catch (InputMismatchException ex) {
 			System.out.println("Wrong input!");
+		} catch (NumberFormatException ex) {
+			System.out.println("You inserted word or symbol");
 		}
 	}
 
